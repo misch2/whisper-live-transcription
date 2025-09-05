dbb05ef29973bccae045d2eb9fcc8d05d1f6fa25
Michal Schwarz <michal.schwarz@gmail.com>
2025-09-05 09:20:44 +0200

    add microphone autoselection

--- standalone-poc/live-transcribe.py
+++ standalone-poc/live-transcribe.py
@@ -15,8 +15,8 @@ from termcolor import colored
 # Yeah I could do this config with argparse, but I won't...
 
 # Audio settings
-STEP_IN_SEC: int = 1    # We'll increase the processable audio data by this
-LENGHT_IN_SEC: int = 6    # We'll process this amount of audio data together maximum
+STEP_IN_SEC: int = 1  # We'll increase the processable audio data by this
+LENGHT_IN_SEC: int = 6  # We'll process this amount of audio data together maximum
 NB_CHANNELS = 1
 RATE = 16000
 CHUNK = RATE
@@ -24,9 +24,11 @@ CHUNK = RATE
 # Whisper settings
 WHISPER_LANGUAGE = "en"
 WHISPER_THREADS = 4
-WHISPER_MODEL = "turbo"    # "base", "small", "medium", "large", "large-v1", "large-v2", "tiny", "tiny.en", "base.en", "small.en", "medium.en", "large-v1.en", "large-v2.en", "turbo"
-#WHISPER_MODEL = "tiny"  # test only
-WHISPER_COMPUTE_TYPE = "float16"    # "int8", "float16", "float32", "int4", "int4_float16", "int4_float32"
+WHISPER_MODEL = "turbo"  # "base", "small", "medium", "large", "large-v1", "large-v2", "tiny", "tiny.en", "base.en", "small.en", "medium.en", "large-v1.en", "large-v2.en", "turbo"
+# WHISPER_MODEL = "tiny"  # test only
+WHISPER_COMPUTE_TYPE = (
+    "float16"  # "int8", "float16", "float32", "int4", "int4_float16", "int4_float32"
+)
 
 # Visualization (expected max number of characters for LENGHT_IN_SEC audio)
 MAX_SENTENCE_CHARACTERS = 80
@@ -48,29 +50,47 @@ home_dir = os.path.expanduser("~")
 
 models_dir = os.path.join(home_dir, ".cache", "huggingface", "hub")
 
-print("Initializing Whisper model '%s' with %d threads and compute type '%s'..." % (WHISPER_MODEL, WHISPER_THREADS, WHISPER_COMPUTE_TYPE))
+print(
+    "Initializing Whisper model '%s' with %d threads and compute type '%s'..."
+    % (WHISPER_MODEL, WHISPER_THREADS, WHISPER_COMPUTE_TYPE)
+)
 print("Models will be downloaded to: %s" % models_dir)
 # Whisper model
 # whisper = WhisperModel("tiny", device="cpu", compute_type="int8", cpu_threads=WHISPER_THREADS, download_root=models_dir)
 # whisper = WhisperModel("turbo", device="cuda", compute_type="float16", cpu_threads=WHISPER_THREADS, download_root=models_dir)
-whisper = WhisperModel(WHISPER_MODEL, device="cuda", compute_type=WHISPER_COMPUTE_TYPE, cpu_threads=WHISPER_THREADS, download_root=models_dir)
+whisper = WhisperModel(
+    WHISPER_MODEL,
+    device="cuda",
+    compute_type=WHISPER_COMPUTE_TYPE,
+    cpu_threads=WHISPER_THREADS,
+    download_root=models_dir,
+)
 
 print("Whisper model initialized")
 
 # Global flag to signal threads to stop
 stop_threads = False
 
+
 def producer_thread():
     audio = pyaudio.PyAudio()
+
+    # list_pyaudio_devices(audio)
+
+    print("-" * 80)
+
+    # Get the default input device index
+    microphone_device_index = get_virtual_audio_mix_device_index(audio)
+
     stream = audio.open(
         format=pyaudio.paInt16,
         channels=NB_CHANNELS,
         rate=RATE,
         input=True,
-        frames_per_buffer=CHUNK,    # 1 second of audio
+        input_device_index=microphone_device_index,
+        frames_per_buffer=CHUNK,  # 1 second of audio
     )
 
-    print("-" * 80)
     print("Microphone initialized, recording started...")
     print("-" * 80)
     print("TRANSCRIPTION using '%s' model" % WHISPER_MODEL)
@@ -80,15 +100,16 @@ def producer_thread():
     while not stop_threads:
         audio_data = b""
         for _ in range(STEP_IN_SEC):
-            chunk = stream.read(RATE)    # Read 1 second of audio data
+            chunk = stream.read(RATE)  # Read 1 second of audio data
             audio_data += chunk
             if stop_threads:
                 break
 
-        audio_queue.put(audio_data)    # Put the 5-second audio data into the queue
+        audio_queue.put(audio_data)  # Put the 5-second audio data into the queue
         # print('(audio)', end='', flush=True)
 
     print("\nStopping audio producer thread...")
+
 
 # Thread which gets items from the queue and prints its length
 def consumer_thread(stats):
@@ -102,7 +123,7 @@ def consumer_thread(stats):
         audio_data = audio_queue.get()
         if stop_threads:
             break
-        
+
         transcription_start_time = time.time()
         length_queue.put(audio_data)
 
@@ -113,16 +134,20 @@ def consumer_thread(stats):
             audio_data_to_process += length_queue.queue[i]
 
         # convert the bytes data toa  numpy array
-        audio_data_array: np.ndarray = np.frombuffer(audio_data_to_process, np.int16).astype(np.float32) / 255.0
+        audio_data_array: np.ndarray = (
+            np.frombuffer(audio_data_to_process, np.int16).astype(np.float32) / 255.0
+        )
         # audio_data_array = np.expand_dims(audio_data_array, axis=0)
 
-        segment_length_seconds = (audio_data_array.shape[0] / RATE)
+        segment_length_seconds = audio_data_array.shape[0] / RATE
         # print("transcribing %d seconds of audio data" % segment_length_seconds)
-        segments, _ = whisper.transcribe(audio_data_array,
-                                         language=WHISPER_LANGUAGE,
-                                         beam_size=5,
-                                         vad_filter=True,
-                                         vad_parameters=dict(min_silence_duration_ms=1000))
+        segments, _ = whisper.transcribe(
+            audio_data_array,
+            language=WHISPER_LANGUAGE,
+            beam_size=5,
+            vad_filter=True,
+            vad_parameters=dict(min_silence_duration_ms=1000),
+        )
         segments = [s.text for s in segments]
 
         transcription_end_time = time.time()
@@ -133,7 +158,6 @@ def consumer_thread(stats):
 
         # print(colored("Raw segments in %d sec segment: %s", "grey"), segment_length_seconds, transcription, end='\r', flush=True)
 
-
         # remove anything from the text which is between () or [] --> these are non-verbal background noises/music/etc.
         # transcription = re.sub(r"\[.*\]", "", transcription)
         # transcription = re.sub(r"\(.*\)", "", transcription)
@@ -143,21 +167,91 @@ def consumer_thread(stats):
         transcription_postprocessing_end_time = time.time()
 
         current_time = time.strftime("%H:%M:%S", time.localtime())
-        print(colored(current_time, "green") + " " + transcription, end='\r', flush=True)
+        print(
+            colored(current_time, "green") + " " + transcription, end="\r", flush=True
+        )
 
         audio_queue.task_done()
 
-        overall_elapsed_time = transcription_postprocessing_end_time - transcription_start_time
+        overall_elapsed_time = (
+            transcription_postprocessing_end_time - transcription_start_time
+        )
         transcription_elapsed_time = transcription_end_time - transcription_start_time
-        postprocessing_elapsed_time = transcription_postprocessing_end_time - transcription_end_time
+        postprocessing_elapsed_time = (
+            transcription_postprocessing_end_time - transcription_end_time
+        )
         stats["overall"].append(overall_elapsed_time)
         stats["transcription"].append(transcription_elapsed_time)
         stats["postprocessing"].append(postprocessing_elapsed_time)
 
     print("\nStopping consumer thread...")
 
+
+def get_virtual_audio_mix_device_index(p):
+    hostapi = p.get_default_host_api_info()
+
+    device_count = p.get_device_count()
+
+    index = 0
+    for i in range(device_count):
+        # Get device info
+        device_info = p.get_device_info_by_index(i)
+        #print(device_info)
+
+        # Check if the device supports input
+        if device_info["maxInputChannels"] > 0 and device_info["hostApi"] == hostapi["index"] and device_info["name"].startswith("Voicemeeter Out B1"):
+        #if device_info["maxInputChannels"] > 0 and device_info["name"].startswith("Voicemeeter Out B1"):
+            print(f"Using default input device index: {i}")
+            print(f"  Name: {device_info['name']}")
+            print(f"  Max Input Channels: {device_info['maxInputChannels']}")
+            print(f"  Default Sample Rate: {device_info['defaultSampleRate']}")
+            index = i
+            break
+
+    return index
+
+
+def list_pyaudio_devices(p):
+    hostapi_count = p.get_host_api_count()
+    print("Host API count: %d" % hostapi_count)
+    for i in range(hostapi_count):
+        # Get host API info
+        hostapi_info = p.get_host_api_info_by_index(i)
+        print(f"Host API ID: {i}")
+        print(f"  Name: {hostapi_info['name']}")
+        print(f"  Device count: {hostapi_info['deviceCount']}")
+        print(f"  Default input device: {hostapi_info['defaultInputDevice']}")
+        print(f"  Default output device: {hostapi_info['defaultOutputDevice']}")
+        print(hostapi_info)
+        print()
+
+    print("Default Host API: %s" % p.get_default_host_api_info())
+    print("Default Input Device: %s" % p.get_default_input_device_info())
+
+    device_count = p.get_device_count()
+
+    for i in range(device_count):
+        # Get device info
+        device_info = p.get_device_info_by_index(i)
+
+        # Check if the device supports input
+        # if device_info["maxInputChannels"] > 0:
+        if device_info["maxInputChannels"] == 1:
+            print(f"Device ID: {i}")
+            print(f"  Name: {device_info['name']}")
+            print(f"  Max Input Channels: {device_info['maxInputChannels']}")
+            print(f"  Default Sample Rate: {device_info['defaultSampleRate']}")
+
+            print(device_info)
+            print()
+
+
 if __name__ == "__main__":
-    stats: Dict[str, List[float]] = {"overall": [], "transcription": [], "postprocessing": []}
+    stats: Dict[str, List[float]] = {
+        "overall": [],
+        "transcription": [],
+        "postprocessing": [],
+    }
 
     producer = threading.Thread(target=producer_thread)
     producer.start()
@@ -165,27 +259,29 @@ if __name__ == "__main__":
     consumer = threading.Thread(target=consumer_thread, args=(stats,))
     consumer.start()
 
-
-    abort_key = ''; #'q'
+    abort_key = ""
+    #'q'
     # print("Press q or Esc to exit")
     try:
         # Wait for 'q' to be pressed
         print("Press '%s' or ^C to stop the threads." % abort_key)
-        if (abort_key == ''):
+        if abort_key == "":
             while not stop_threads:
                 time.sleep(0.1)
         else:
             keyboard.wait(abort_key)
             print("Exiting due to abort key (%s) pressed" % abort_key)
             stop_threads = True
-            
+
     except KeyboardInterrupt as e:
         print("Exiting due to keyboard interrupt: %s" % str(e))
         stop_threads = True
 
     # print out the statistics
     print("Number of processed chunks: ", len(stats["overall"]))
-    print(f"Overall time: avg: {np.mean(stats['overall']):.4f}s, std: {np.std(stats['overall']):.4f}s")
+    print(
+        f"Overall time: avg: {np.mean(stats['overall']):.4f}s, std: {np.std(stats['overall']):.4f}s"
+    )
     print(
         f"Transcription time: avg: {np.mean(stats['transcription']):.4f}s, std: {np.std(stats['transcription']):.4f}s"
     )
